dd46b0ee5a507e441c686c26280779b9773ead93
Harshit Ashri <134440516+HaRsHiTaShRi2022@users.noreply.github.com>
2025-03-10 23:53:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash\
 
 echo "Installing LibreOffice..."
-apt-get update && apt-get install -y libreoffice
+apt-get update && apt-get install -y libreoffice libreoffice-common libreoffice-core
 
 echo "Starting Gunicorn..."
 gunicorn Main:app
